2e79f44cb5e1fa194edce42ae9c0bd8dcdb4b353
Logan Wemyss <10540653-VixieTSQ@users.noreply.gitlab.com>
2022-12-19 19:54:18 -0700

    mommy has all sorts of different things to tell you~

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use rand::{rngs::StdRng, seq::IteratorRandom, SeedableRng};
 use std::process::ExitCode;
+
+const NEGATIVE_RESPONSES: &str = include_str!("../responses/negative-responses.txt");
+const POSITIVE_RESPONSES: &str = include_str!("../responses/positive-responses.txt");
+const AFFECTIONATE_TERM_PLACEHOLDER: &str = "AFFECTIONATE_TERM";
 
 fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
     let cargo = std::env::var("CARGO")?;
@@ -12,12 +17,23 @@ fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
     let status = cmd.status()?;
     eprintln!("\x1b[1m");
     if status.success() {
-        eprintln!("Good {}~\nMommy's so proud of you~ ❤️", mommys_little);
+        eprintln!("{}", select_response(POSITIVE_RESPONSES, mommys_little))
     } else {
-        eprintln!("Mommy knows her little {} can do better~ ❤️", mommys_little);
+        eprintln!("{}", select_response(NEGATIVE_RESPONSES, mommys_little));
     }
     eprintln!("\x1b[0m");
     Ok(ExitCode::from(status.code().unwrap_or(-1) as u8))
+}
+
+fn select_response(responses: &str, affectionate_term: String) -> String {
+    let mut rng = StdRng::from_entropy();
+    let response = responses
+        .lines()
+        .choose(&mut rng)
+        .expect("non-zero amount of responses")
+        .replace(AFFECTIONATE_TERM_PLACEHOLDER, &affectionate_term);
+
+    return response;
 }
 
 #[cfg(test)]
