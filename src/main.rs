df25c6ef88f154ef0f3e0cda8a54fae55e32704f
Logan Wemyss <10540653-VixieTSQ@users.noreply.gitlab.com>
2022-12-19 22:30:37 -0700

    mommy looooves serialization

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
-use rand::{rngs::StdRng, seq::IteratorRandom, SeedableRng};
+use rand::{rngs::StdRng, seq::SliceRandom, SeedableRng};
+use serde::Deserialize;
 use std::process::ExitCode;
 
-const NEGATIVE_RESPONSES: &str = include_str!("../responses/negative-responses.txt");
-const POSITIVE_RESPONSES: &str = include_str!("../responses/positive-responses.txt");
-const AFFECTIONATE_TERM_PLACEHOLDER: &str = "AFFECTIONATE_TERM";
+const RESPONSES: &'static str = include_str!("../responses.json");
+const AFFECTIONATE_TERM_PLACEHOLDER: &'static str = "AFFECTIONATE_TERM";
+
+#[derive(Deserialize)]
+struct Responses {
+    positive: Vec<String>,
+    negative: Vec<String>,
+}
+
+enum ResponseType {
+    Positive,
+    Negative,
+}
 
 fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
     let cargo = std::env::var("CARGO")?;
-    let mommys_little = std::env::var("CARGO_MOMMYS_LITTLE").unwrap_or_else(|_| "girl".to_owned());
     let mut arg_iter = std::env::args();
     let _cargo = arg_iter.next();
     let _mommy = arg_iter.next();
@@ -17,23 +27,27 @@ fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
     let status = cmd.status()?;
     eprintln!("\x1b[1m");
     if status.success() {
-        eprintln!("{}", select_response(POSITIVE_RESPONSES, mommys_little))
+        eprintln!("{}", select_response(ResponseType::Positive))
     } else {
-        eprintln!("{}", select_response(NEGATIVE_RESPONSES, mommys_little));
+        eprintln!("{}", select_response(ResponseType::Negative));
     }
     eprintln!("\x1b[0m");
     Ok(ExitCode::from(status.code().unwrap_or(-1) as u8))
 }
 
-fn select_response(responses: &str, affectionate_term: String) -> String {
+fn select_response(response_type: ResponseType) -> String {
+    let affectionate_term =
+        std::env::var("CARGO_MOMMYS_LITTLE").unwrap_or_else(|_| "girl".to_owned());
     let mut rng = StdRng::from_entropy();
-    let response = responses
-        .lines()
-        .choose(&mut rng)
-        .expect("non-zero amount of responses")
-        .replace(AFFECTIONATE_TERM_PLACEHOLDER, &affectionate_term);
+    let responses: Responses = serde_json::from_str(RESPONSES).expect("RESPONSES to be valid JSON");
 
-    return response;
+    return match response_type {
+        ResponseType::Positive => responses.positive,
+        ResponseType::Negative => responses.negative,
+    }
+    .choose(&mut rng)
+    .expect("non-zero amount of responses")
+    .replace(AFFECTIONATE_TERM_PLACEHOLDER, &affectionate_term);
 }
 
 #[cfg(test)]
