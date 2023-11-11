947b5aa751eff67976b629bed7f183799b63cd30
5225225 <5225225@mailbox.org>
2023-11-11 16:04:07 +0000

    Add features to not emit moods at build time

--- build.rs
+++ build.rs
@@ -16,44 +16,44 @@ struct Responses {
     negative: Vec<String>,
 }
 
+fn serialise_responses(name: &str, response: Responses) -> String {
+    let response_positive = response.positive;
+    let response_negative = response.negative;
+
+    format!(
+        r#"("{name}", &[
+        &{response_positive:?},
+        &{response_negative:?},
+    ]),"#
+    )
+}
+
 fn main() {
     let out_dir = &env::var("OUT_DIR").unwrap();
     let AllResponses {
-        chill:
-            Responses {
-                positive: chill_positive,
-                negative: chill_negative,
-            },
-        thirsty:
-            Responses {
-                positive: thirsty_positive,
-                negative: thirsty_negative,
-            },
-        yikes:
-            Responses {
-                positive: yikes_positive,
-                negative: yikes_negative,
-            },
+        chill,
+        thirsty,
+        yikes,
     } = serde_json::from_str(RESPONSES).unwrap();
     let dest_path = Path::new(out_dir).join("responses.rs");
+
+    let mut enabled_responses = String::new();
+
+    enabled_responses += &serialise_responses("chill", chill);
+
+    if cfg!(feature = "thirsty") {
+        enabled_responses += &serialise_responses("thirsty", thirsty);
+    }
+    if cfg!(feature = "yikes") {
+        enabled_responses += &serialise_responses("yikes", yikes);
+    }
 
     fs::write(
         dest_path,
         format!(
             r#"const RESPONSES: &[(&str, &[&[&str]])] = &[
-    ("chill", &[
-        &{chill_positive:?},
-        &{chill_negative:?},
-    ]),
-    ("thirsty", &[
-        &{thirsty_positive:?},
-        &{thirsty_negative:?},
-    ]),
-    ("yikes", &[
-        &{yikes_positive:?},
-        &{yikes_negative:?},
-    ]),
-];"#
+                {enabled_responses}
+            ];"#
         ),
     )
     .unwrap();
